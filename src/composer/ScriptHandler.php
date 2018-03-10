427a559c0969542687d789e2851eb1393b8d5144
Matthieu SCARSET <m@matthieuscarset.com>
2018-03-10 11:37:24 -0500

    Update theme assets build with realpath and NPM

--- src/composer/ScriptHandler.php
+++ src/composer/ScriptHandler.php
@@ -10,23 +10,32 @@ namespace DrupalPyramidTheme;
 class ScriptHandler {
 
   /**
-   *
+   * Get current theme path.
+   */
+  private static function themePath() {
+    // $themePath = "./web/themes/contrib/drupal_pyramid_theme/";
+    $themePath = realpath(__DIR__ . '/../..');
+    return $themePath;
+  }
+
+  /**
    * Install npm and build assets.
-   *
-   * @return void
    */
   public static function build() {
     $env = (getenv('ENV') === 'production') ? '--production' : '';
-    $themePath = "./web/themes/contrib/drupal_pyramid_theme/";
+    $themePath = static::themePath();
     passthru("echo '==============================='");
     passthru("echo 'Generating Pyramid theme assets'");
     passthru("echo '==============================='");
     passthru("cd {$themePath} && npm install {$env} && npm run build:all");
   }
-  
+
+  /**
+   * Update assets.
+   */
   public static function update() {
     $env = (getenv('ENV') === 'production') ? '--production' : '';
-    $themePath = "./web/themes/contrib/drupal_pyramid_theme/";
+    $themePath = static::themePath();
     passthru("echo '==============================='");
     passthru("echo 'Updating Pyramid theme assets'");
     passthru("echo '==============================='");
